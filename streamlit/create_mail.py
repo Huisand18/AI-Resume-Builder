364abf4cd46eb4dcd0d16ad05d6015f406af9813
Huisand18 <71809399+Huisand18@users.noreply.github.com>
2024-02-02 20:43:41 +0700

    Update create_mail.py

--- streamlit/create_mail.py
+++ streamlit/create_mail.py
@@ -72,26 +72,7 @@ st.sidebar.markdown(
 )
 
 user_input = st.text_area("Prompt:", max_chars=2000)
-language_choices = {
-    "Indonesian": "id",
-    "English": "en",
-    "Spanish": "es",
-    "French": "fr",
-    "Hindi": "hi",
-    "Russian": "ru",
-    "Italian": "it",
-    "Portuguese": "pt",
-    "Arabic": "ar",
-    "Mandarin": "zh-CN",
-}
-
-selected_language = st.selectbox("Select Language for Translation:", list(language_choices.keys()))
-language_code = language_choices[selected_language]
-if language_code == "zh-CN":
-    selected_language_display = "Mandarin"
-else:
-    selected_language_display = selected_language
-
+language_choice = st.selectbox("Select Language for Translation:", ("Indonesian", "English", "Spanish", "French", "Hindi", "Russian", "Italian", "Portuguese", "Arabic", "Mandarin": "zh-CN"))
 submit_button = st.button("Submit")
 download_button = False
 
